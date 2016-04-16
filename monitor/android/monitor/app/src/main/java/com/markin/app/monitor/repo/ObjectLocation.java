553610672c0fe6ce742bad3a99f27b16527f1eec
Sri Senthil Kumar Alagumalai <salagumalai@altimetrik.com>
2016-04-17 03:07:30 +0530

    Server and monitor updated

---
+++ monitor/android/monitor/app/src/main/java/com/markin/app/monitor/repo/ObjectLocation.java
@@ -0,0 +1,7 @@
+package com.markin.app.monitor.repo;
+
+/**
+ * Created by salagumalai on 16-04-2016.
+ */
+public class ObjectLocation {
+}
---
+++ monitor/android/monitor/app/src/main/java/com/markin/app/monitor/util/RestClient.java
@@ -0,0 +1,7 @@
+package com.markin.app.monitor.util;
+
+/**
+ * Created by salagumalai on 17-04-2016.
+ */
+public class RestClient {
+}
